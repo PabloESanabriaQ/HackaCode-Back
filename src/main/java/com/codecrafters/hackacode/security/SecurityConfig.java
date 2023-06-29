c4d3d75f90fabefe432d8ac221ca20205187bbf7
SantiMolina9 <santiagomolina179@gmail.com>
2023-06-28 22:54:52 -0300

    ✨ Add Security config

--- src/main/java/com/codecrafters/hackacode/security/SecurityConfig.java
+++ src/main/java/com/codecrafters/hackacode/security/SecurityConfig.java
@@ -9,13 +9,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
 public class SecurityConfig {
     @Autowired
@@ -35,6 +36,7 @@ public class SecurityConfig {
                 .csrf(config -> config.disable())
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/api/personas").permitAll();
+                    auth.requestMatchers("/api/juegos").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .sessionManagement(session -> {
