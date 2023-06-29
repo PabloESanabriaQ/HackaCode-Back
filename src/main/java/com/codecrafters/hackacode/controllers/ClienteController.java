eeb92e771e5cace05295af8f88c364f2bd4351f5
SantiMolina9 <santiagomolina179@gmail.com>
2023-06-28 22:43:31 -0300

    ✨ Add Annotacion PreAuthorize for authorization

--- src/main/java/com/codecrafters/hackacode/controllers/ClienteController.java
+++ src/main/java/com/codecrafters/hackacode/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package com.codecrafters.hackacode.controllers;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.codecrafters.hackacode.models.Cliente;
@@ -10,6 +11,7 @@ import com.codecrafters.hackacode.services.ClienteService;
 
 @RestController()
 @RequestMapping(value = "/api/clientes")
+@PreAuthorize("hasRole('ADMINISTRADOR')")
 public class ClienteController {
     
     @Autowired
