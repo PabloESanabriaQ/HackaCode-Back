2c7324749224b873c561ace7e6fff7ed30614f39
SantiMolina9 <santiagomolina179@gmail.com>
2023-06-28 22:56:29 -0300

    ✨ Add feature

--- src/main/java/com/codecrafters/hackacode/HackacodeApplication.java
+++ src/main/java/com/codecrafters/hackacode/HackacodeApplication.java
@@ -1,16 +1,7 @@
 package com.codecrafters.hackacode;
-import com.codecrafters.hackacode.models.ERol;
-import com.codecrafters.hackacode.models.Rol;
-import com.codecrafters.hackacode.models.Usuario;
-import com.codecrafters.hackacode.repositories.IUsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Set;
 
 @SpringBootApplication
 public class HackacodeApplication {
